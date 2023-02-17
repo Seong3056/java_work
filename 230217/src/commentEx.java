826cacdd812b74986c349a0a2dee33df7aca2865
Seong <aktnf207@naver.com>
2023-02-17 17:23:18 +0900

    day1 java_work

---
+++ 230217/src/commentEx.java
@@ -0,0 +1,30 @@
+/**
+ * 문서 설명용 주석
+ * @author ict03-13
+ * @version 1.0
+ * @since 23 02 17~
+ */
+//이 파일은 주석을 연습하는 파일입니다.
+public class commentEx {
+	public static void main(String[] args) {
+
+	
+	/*
+	# main 함수(메서드)는 프로그램 실행 시 자동으로 동작하는 함수이며,
+	반드시 필요한 함수입니다.
+	
+	# 자바 코드를 해석하여 실행시키는 자바 가상머신(jvm)은 
+	프로그램 실행 시 자동으로 main함수를 불러서 내부의 코드를
+	실행시킵니다.
+	*/
+		
+	//syso Ctrl + Space : 빠른 단축 함수
+	//Ctrl + F11 : 실행 단축키
+	//원하는 위치에 커서 배치후 Ctrl + / 문장 주석 처리, 해제
+		
+	System.out.println("안녕하세요~");
+	System.out.println("반갑습니다~");
+	
+	}
+
+}
